b133d545c4afeac4f0c3fd530a811263e097b1b5
NatsirtDml <tristan.doumalin@gmail.com>
2023-10-11 12:20:46 +0000

    first todolist program

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,96 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="Header">
+      <h1>To Do List Doums</h1>
+      <img
+        class="Logo"
+        src="https://c8.alamy.com/comp/2BYBEAY/doodle-to-do-list-icon-or-logo-hand-drawn-with-thin-black-line-2BYBEAY.jpg"
+        alt="AU BOULOT"
+        width="200"
+        height="200"
+      />
+    </div>
+    <input type="text" name="todo" id="todo" placeholder="Ajouter un ToDo" />
+    <button type="submit" @click="addTodo">Add</button>
+    <div>
+      <ToDo
+        v-for="element in list"
+        :key="element.id"
+        :id="element.id"
+        :text="element.todo"
+        :checked="element.checked"
+        @delete="deleteToDo"
+        @checked="check"
+      ></ToDo>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import ToDo from "./components/ToDo.vue";
+import axios from "axios";
 export default {
+  data() {
+    return {
+      newToDo: "",
+      list: [],
+    };
+  },
+
   name: "App",
   components: {
-    HelloWorld,
+    ToDo,
+  },
+
+  methods: {
+    // addTodo() {
+    //   const newToDo = document.getElementById("todo").value;
+    //   this.list.push(newToDo);
+    // },
+
+    async addTodo() {
+      const text = document.getElementById("todo").value;
+      const response = await axios.put("http://localhost:3000/todo", {
+        todo: text,
+      });
+      this.list.push({ id: response.data[0], todo: text, checked: 0 });
+    },
+
+    async deleteToDo(id) {
+      await axios.delete(`http://localhost:3000/todo/${id}`);
+      const textDelete = this.list.findIndex((element) => element.id === id);
+      this.list.splice(textDelete, 1);
+    },
+
+    async check({ id, value }) {
+      await axios.post(`http://localhost:3000/todo/check/${id}/${value}`);
+    },
+  },
+
+  async mounted() {
+    const response = await axios.get("http://localhost:3000/todo");
+    this.list = response.data;
   },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Comic Sans MS", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.Header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.Logo {
+  position: absolute;
+  right: 0%;
+  top: 0.5%;
 }
 </style>
---
+++ src/components/ToDo.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="general">
+    <div class="box">
+      {{ text }}
+    </div>
+    <div>
+      <input type="checkbox" name="check" id="check" v-model="check" />
+      <button @click="deleteToDo">
+        <img
+          src="https://static.vecteezy.com/system/resources/previews/016/775/743/non_2x/red-cross-isolated-png.png"
+          alt="supprimer"
+          width="10px"
+          height="10px"
+        />
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ToDo",
+  computed: {
+    check: {
+      set(value) {
+        this.$emit("checked", { id: this.id, value });
+      },
+      get() {
+        return this.checked;
+      },
+    },
+  },
+  props: {
+    text: String,
+    checked: Number,
+    id: Number,
+  },
+
+  methods: {
+    deleteToDo() {
+      this.$emit("delete", this.id);
+    },
+  },
+};
+</script>
+
+<style>
+.general {
+  display: flex;
+}
+.box {
+  border: 1px solid black;
+  width: 60%;
+}
+</style>
